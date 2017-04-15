5e95a09611570c20ddb272fa145f780021e94c32
nichabalie <nichabalie@poste.isima.fr>
2017-04-14 21:02:27 -0400

    Initial commit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import javax.annotation.processing.SupportedSourceVersion;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        System.out.println("Coucou");
+        Referee ref = new Referee(System.in, System.out, System.err);
     }
 }
